212620807e10dbe279c77e4edd5724d0eb79532a
cwhite098 <gd19031@bristol.ac.uk>
2022-02-18 20:30:12 +0000

    got shooting working, need to make nice, add comments and make sure it works generally

--- numerical_shooting.py
+++ numerical_shooting.py
@@ -7,20 +7,25 @@ from ode import predator_prey
 
 def numerical_shooting(X0):
 
-    T = 20
+    T = X0[-1]
+    X0 = X0[:-1]
     t = np.linspace(0,T,3)
 
     solution = solve_ode('rk4', predator_prey, t, X0, a=1, b=0.2, d=0.1, hmax=0.001)
     
-    output = X0 - solution[-1,:]
+    output = np.append(X0 - solution[-1,:], phase_condition(X0, a=1, b=0.2, d=0.1))
 
     return output
 
 
+def phase_condition(X0, **params):
+    return predator_prey(X0, 0, params)[0]
+
+
 def find_vars():
 
-    X0=[0.1, 1]
-    sol = fsolve(numerical_shooting, X0 )
+    X0=[1.5, 1.5, 10]
+    sol = fsolve(numerical_shooting, X0)
 
     return sol
 
