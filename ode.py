167979bd803d15bd2bcf1e26deee52a0e54058e6
cwhite098 <gd19031@bristol.ac.uk>
2022-02-21 18:51:28 +0000

    added 2d and 3d hopf ODEs

--- ode.py
+++ ode.py
@@ -32,3 +32,37 @@ def predator_prey(X, t, params):
     X = [dxdt, dydt]
 
     return X
+
+
+def hopf(X, t, params):
+
+    beta = params['beta']
+    sigma = params['sigma']
+
+    u1 = X[0]
+    u2 = X[1]
+
+    du1dt = beta*u1 - u2 + sigma*u1*(u1**2 + u2**2)
+    du2dt = u1 + beta*u2 + sigma*u2*(u1**2 + u2**2)
+
+    X = [du1dt, du2dt]
+
+    return X
+
+
+def hopf3D(X, t, params):
+
+    beta = params['beta']
+    sigma = params['sigma']
+
+    u1 = X[0]
+    u2 = X[1]
+    u3 = X[2]
+
+    du1dt = beta*u1 - u2 + sigma*u1*(u1**2 + u2**2)
+    du2dt = u1 + beta*u2 + sigma*u2*(u1**2 + u2**2)
+    du3dt = -u3
+
+    X = [du1dt, du2dt, du3dt]
+
+    return X
