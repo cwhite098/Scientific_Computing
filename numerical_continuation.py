ffe4b5400241dfed85fa4af6c57637e82a8fd9ad
cwhite098 <gd19031@bristol.ac.uk>
2022-03-09 19:37:54 +0000

    added doc strings and cleaned up functions, main needs tidying and tests need to be written

--- numerical_continuation.py
+++ numerical_continuation.py
@@ -106,90 +106,220 @@ def natural_param_continuation(initial_u, param_to_vary, param_range, no_param_v
 
 
 def get_arc(u3, u2, u1, param3, param2, param1):
+    '''
+    Function that returns the dot product between the linear prediction of the next point in the
+    numerical continuation and the vector between the prediction and the proposed next point provided
+    by the root finding algorithm. This is the pseudo-arclength condition.
 
-    # Why is pred == target????????
+    Parameters
+    ----------
+    u3 : np.array
+        Current state vector that is being solved for by the root finding method.
+    
+    u2 : np.array
+        State vector computed for iteration t-1 (where t is the current iteration).
+
+    u1 : np.array
+        State vector computed for iteration t-2 (where t is the current iteration).
+    
+    param3 : float
+        Current parameter value that is being solved for by the root finding method.
+
+    param2 : float
+        Parameter value for iteration t-1 (where t is the current iteration).
+
+    param1 : float
+        Parameter value for iteration t-2 (where t is the current iteration).
+    
+    Returns
+    -------
+    arc : float
+        The dot product to be set to 0 by the root finding method. It is the dot product between
+        the secant and the vector joining [u3,p3] and [u2,p2].
+    '''
+    # Calculate the (linear prediction of next (u,p))
     pred = np.append(u2 + (u2 - u1), param2 + (param2 - param1))
+    # Create vector of the proposed correction
     target = np.append(u3, param3)
-    # Direction of natural continuation
+    # Calculate secant
     secant = np.append((u2-u1), (param2-param1))
 
+    # Take the dot product to test for orthogonality
     arc = np.dot(target - pred, secant)
 
     return arc
         
-def root_finding(x, discretisation, function, u1, u2, p1, p2, param_to_vary, phase_condition, T_guess,  params):
+def root_finding(x, discretisation, function, u1, u2, p1, p2, param_to_vary, phase_condition, params):
+    '''
+    Function that forms the root finding problem to be solved by the pseudo-arclength numerical
+    continuation method.
     
-    # Predictions (natural cont.)
+    Parameters
+    ----------
+
+    x : np.array
+        Array containing the current guess at the solution to the root finding problem. This contains
+        the system state variables augmented with the current parameter value.
+    
+    discretisation : function
+        For an ODE this is the numerical shooting function (numerical_shooting).
+        For some function (not ODE) this is simply lambda x:x which is the default.
+
+    function : function
+        The function containing the system the numerical continuation is being applied to.
+
+    u1 : np.array
+        State vector computed for iteration t-2 (where t is the current iteration).
+
+    u2 : np.array
+        State vector computed for iteration t-1 (where t is the current iteration).
+
+    param1 : float
+        Parameter value for iteration t-2 (where t is the current iteration).
+
+    param2 : float
+        Parameter value for iteration t-1 (where t is the current iteration).
+
+    param_to_vary : string
+        The name of the parameter that is being varied in the numerical continuation.
+
+    phase_condition : function
+        If the system is an ODE sysetm, a phase condition is required to create the root finding
+        problem. Pass None value is system is not an ODE system.
+
+    params : dict
+        The parameters required to solve the system, including the parameter that is
+        being varied.
+
+    Returns
+    -------
+    root : np.array
+        The combination of the discretisation output and the pseudo-arclength condition.
+        This quantity is being solved for by the root finder.
+    '''
+    # Unpack the variables from the function input
     u0 = x[:-1]
     p0 = x[-1]
+    # Set the parameter value to be passed to the function
     params[param_to_vary] = p0
+
+    # If the problem involves an ODE
     if discretisation == numerical_shooting:
         d = root_finding_problem(list(u0), function, phase_condition, params)
-        #d = d[:-1]
-
-        arc = get_arc(u0, u2, u1, p0, p2, p1)
-        root = np.append(d, arc)
-        
+    # If the problem involves a non-ODE equation
     else:
         d = discretisation(function(u0, params))
-        arc = get_arc(u0, u2, u1, p0, p2, p1)
-        root = np.append(d, arc)
+    # Add the pseudo-arclength condition    
+    arc = get_arc(u0, u2, u1, p0, p2, p1)
+    root = np.append(d, arc)
     
     return root
 
 
-
-
 def pseudo_arclength_continuation(initial_u, param_to_vary, param_range, no_param_values, function, discretisation=lambda x:x,
-                                    solver='fsolve', phase_condition = None, T_guess = 5, **params):
+                                    phase_condition = None, T_guess = 5, **params):
+    '''
+    Function that carries out the pseudo-arclength numerical continuation method by constructing a root
+    finding problem using either numerical shooting (for ODEs) or a simple discretisation (for non-ODEs)
+    as well as the pseudo-arclength condition.
 
+    Parameters
+    ----------
+    initial_u : list
+        List containing the initial conditions for the system.
+
+    param_to_vary : string
+        The name of the parameter to be varied in the numerical continuation.
+
+    param_range : list
+        List of length 2 that contains the bounds within which the parameter is to be varied.
+
+    no_param_values : int
+        The number of different parameter values to solve the system for. This controls the
+        resolution of the output continuation.
+
+    function : function
+        Function containing the system the continuation is to be applied to.
+
+    discretisation : function
+        Function that forms the root finding problem along with the supplied system and
+        the pseudo-arclength condition.
+
+    phase_condition : function
+        If the system is an ODE system, a phase condition is required to from the root finding
+        problem for the numerical shooting process.
+
+    T_guess : float
+        Initial guess for the period of the orbits being found.
+
+    **params:
+        Any parameters required to compute the output of the system.
+
+    Returns
+    -------
+    sols : np.array
+        Array containing the solutions to the system for each parameter value.
+
+    param_list : list
+        List containing all the parameter values that have been used to evaluate the system.
+
+    Example
+    -------
+        u, p = pseudo_arclength_continuation([1,1], 'beta', [2,-1], 100, modified_hopf,
+                                                numerical_shooting, phase_condition=pc_modified_hopf, T_guess=6, beta=2)
+    '''
     # Form param list and retrieve first 2 entries to begin continuation
     param_list = np.linspace(param_range[0], param_range[1], no_param_values)
     param_list = list(param_list[:2])
+
+    # Save the initial system state and initial guess at orbit period.
     sols = [initial_u]
     Ts = [T_guess]
-    counter=0
 
-    # Figure out this bool thing
+    # While the parameter lies within the specified range, continue
     while np.min(param_range) <= param_list[-1] and param_list[-1] <= np.max(param_range):
-        counter+=1
+        
+        # Generate the first 2 system states and parameter values
         if len(sols)==1:
             for i in range(2):
+                # Set parameter
                 param1 = param_list[-2+i]
                 params[param_to_vary] = param1
+                # If ODE system
                 if discretisation == numerical_shooting:
                     X0, T = numerical_shooting(sols[-1].copy(), Ts[-1], function, phase_condition, **params)
                     u1 = list(X0)
                     Ts.append(T)
+                # If non-ODE system
                 else:
                     u1 = fsolve(discretisation(function), initial_u, args=params)
                 sols.append(list(u1))
-            u1 = np.array(sols[-2])
-            u2 = np.array(sols[-1])
-            param1 = param_list[-2]
-            param2 = param_list[-1]
-        else:
-            u1 = np.array(sols[-2])
-            u2 = np.array(sols[-1])
-            param1 = param_list[-2]
-            param2 = param_list[-1]
 
-        
+        # Get system states and param values for next continuation step      
+        u1 = np.array(sols[-2])
+        u2 = np.array(sols[-1])
+        param1 = param_list[-2]
+        param2 = param_list[-1]
+    
+        # Add period to state vector is searching for orbits
         if discretisation == numerical_shooting:
             u1 = np.append(u1, Ts[-2])
             u2 = np.append(u2, Ts[-1])
-            pred = np.append(u2 + (u2 - u1), param2+(param2-param1))
-            
+            pred = np.append(u2 + (u2 - u1), param2+(param2-param1)) 
         else:
             pred = np.append(u2 + (u2 - u1),param2+(param2-param1))
 
+        # Solve root finding problem and store the results
         x = fsolve(root_finding, np.array(pred),
-                    args = (discretisation, function, u1, u2, param1, param2, param_to_vary, phase_condition, T_guess, params))
+                    args = (discretisation, function, u1, u2, param1, param2, param_to_vary, phase_condition, params))
 
         sols.append(list(x[:len(initial_u)]))
+        Ts.append(x[-2])
         param_list.append(x[-1])
-        
-    return np.array(sols[1:]), param_list
+    
+    # Remove initial_u from solution vector
+    sols = np.array(sols[1:])
+    return sols, param_list
 
 
 def main():
@@ -225,8 +355,6 @@ def main():
     u, p = pseudo_arclength_continuation([-1,-1], 'beta', [2,0], 50, hopf, numerical_shooting, phase_condition=pc_hopf, T_guess=6, beta=-1, sigma=-1)
     plt.plot(p,u[:,0])
     plt.show()
-
-
 
     u, p = natural_param_continuation([1,1], 'beta', [2,-1], 20, modified_hopf, numerical_shooting, phase_condition=pc_modified_hopf, T_guess=6, beta=-1)
     plt.plot(p,u[:,0])
